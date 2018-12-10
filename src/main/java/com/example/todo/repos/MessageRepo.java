f0d340441ce7f9a86b57c45d67fd9df00f2d8472
yaroslavchuiev <yaroslav.chuiev@outlook.com>
2018-12-10 16:14:46 +0100

    First Version

---
+++ src/main/java/com/example/todo/repos/MessageRepo.java
@@ -0,0 +1,12 @@
+package com.example.todo.repos;
+
+import com.example.todo.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public
+    interface MessageRepo
+    extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
+}
