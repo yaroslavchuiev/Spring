52a1466319879e4774b2da4169cbf6df96538aaa
yaroslavchuiev <yaroslav.chuiev@outlook.com>
2018-12-11 01:51:55 +0100

    First Version

--- src/main/java/com/example/todo/repos/MessageRepo.java
+++ src/main/java/com/example/todo/repos/MessageRepo.java
@@ -8,5 +8,5 @@ import java.util.List;
 public
     interface MessageRepo
     extends CrudRepository<Message, Long> {
-    List<Message> findByTag(String tag);   
+    List<Message> findByTag(String tag);
 }
